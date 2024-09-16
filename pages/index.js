130ecae58540f07ea57c7e3ed74322a78cb2f9a8
Gabriel Ramalhao <41525935+gabrielramalhao@users.noreply.github.com>
2024-09-16 11:53:30 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Agora me diga que está me devendo.</h1>
+  return <h1>Agora me diga que está me devendo.</h1>;
+}
+
+function teste() {
+  console.log("teste errado");
 }
 
 export default Home;
