f44a9f22a3b40889d059502224fd66bff7d651e5
Gabriel Ramalhao <41525935+gabrielramalhao@users.noreply.github.com>
2024-09-18 23:49:19 +0000

    mensagem para a izabella

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Agora me diga que está me devendo.</h1>;
+  return <h1>Izabella Maria de Morais, eu amo você mais do que tudo no mundo.</h1>;
 }
 
 export default Home;
